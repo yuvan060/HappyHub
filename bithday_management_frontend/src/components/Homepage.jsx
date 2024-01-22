450b4cac8b870656d064f2fbe6145a29115e925b
yuvan060 <yuvansankar060@gmail.com>
2024-01-22 14:33:22 +0530

    intial commit

---
+++ bithday_management_frontend/src/components/Homepage.jsx
@@ -0,0 +1,12 @@
+import PrimarySearchAppBar from "./Nav_bar";
+
+function Homepage() {
+  return (
+    <>
+      <PrimarySearchAppBar />
+      <h1>Hello U1 !!!</h1>
+    </>
+  );
+}
+
+export default Homepage;
